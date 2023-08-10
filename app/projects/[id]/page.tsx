b4b6b59a1afb7b00f5674e46e125839e3d572a94
zhuima <zhuima314@gmail.com>
2023-08-10 11:58:36 +0800

    update project details page

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -2,29 +2,29 @@
  * @Author: zhuima zhuima314@gmail.com
  * @Date: 2023-05-10 15:05:55
  * @LastEditors: zhuima zhuima314@gmail.com
- * @LastEditTime: 2023-07-28 16:53:18
- * @FilePath: /ruankao-website/app/videos/[id]/page.tsx
+ * @LastEditTime: 2023-08-10 11:57:08
+ * @FilePath: /nextjs-roadmap/app/projects/[id]/page.tsx
  * @Description: https://www.bilibili.com/video/BV1Ce411N7pV/
  *
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
  */
 import Image from "next/image";
 import Link from "next/link";
-import videosData from "@/config/videos";
+import projectsData from "@/config/projects";
 import { DocsPageHeader } from "@/components/page-header";
 
-function filterVideos(videos, id) {
-  return videos.find((video) => video.id === id);
+function filterProjects(projects, id) {
+  return projects.find((project) => project.id === id);
 }
 
 export default function Page({ params }) {
-  const videoData = filterVideos(videosData, params.id);
+  const projectData = filterProjects(projectsData, params.id);
 
-  console.log("videoData", videoData);
+  console.log("videoData", projectData);
   return (
     <div className="flex min-h-screen flex-col">
       <div className="container py-12">
-        <DocsPageHeader heading="视频详情" text="." />
+        <DocsPageHeader heading="项目详情" text="." />
       </div>
 
       <div className="container">
